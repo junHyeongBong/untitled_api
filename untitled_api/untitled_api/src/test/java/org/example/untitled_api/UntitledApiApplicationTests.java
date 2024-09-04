63f128dfe820d1f6661f8e8225ca32df88e87ff8
dante <heywheredante@gmail.com>
2024-09-04 22:44:47 +0900

    test: 소스 교체

---
+++ untitled_api/untitled_api/src/test/java/org/example/untitled_api/UntitledApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.untitled_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
