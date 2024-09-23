84d9285bacc7dec5fbd2c5d6d50434881221f808
dante <heywheredante@gmail.com>
2024-09-23 22:29:17 +0900

    feat:test코드

--- untitled_api/unknownProject/src/test/java/test/test.java
+++ untitled_api/unknownProject/src/test/java/test/test.java
@@ -2,12 +2,24 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class test {
 
     @Test
     public void test234(){
+
+        File file = new File("/Users/bongjunhyeong/Desktop/코딩관련/test");
+
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
         System.out.println("test");
+
     }
+
+
 
 
 
