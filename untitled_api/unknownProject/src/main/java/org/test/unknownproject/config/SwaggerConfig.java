ed58418bdc94ae19d3ee62fd5151407dc0e9ee17
dante <heywheredante@gmail.com>
2024-10-23 23:25:42 +0900

    refactor: swagger코드 변경

--- untitled_api/unknownProject/src/main/java/org/test/unknownproject/config/SwaggerConfig.java
+++ untitled_api/unknownProject/src/main/java/org/test/unknownproject/config/SwaggerConfig.java
@@ -1,26 +1,39 @@
 package org.test.unknownproject.config;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "index", description = "index ㅍㅔ이지 들어갈 내용"))
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = getSecurityScheme();
+        SecurityRequirement securityRequirement = getSecurityRequireMent();
+
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement));
     }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("test Swagger")
-                .description("unknownProject test")
-                .version("1.0.0");
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
     }
+
+    private SecurityRequirement getSecurityRequireMent() {
+        return new SecurityRequirement().addList("bearerAuth");
+    }
+
+
 
 }
